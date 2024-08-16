409608fb5e736e729f74acfeeffc487e6575458b
Mads Holten Rasmussen <mads.h.rasmussen@gmail.com>
2024-08-16 11:23:52 +0200

    Started deck on knowledge graphs

--- dist/html/cytoscape/helpers.js
+++ dist/html/cytoscape/helpers.js
@@ -9,7 +9,7 @@ export function logNodePositions(cy){
         graphJSON.elements.nodes.forEach((n) => {
             const obj = {};
             obj.data = n.data;
-            obj.position = n.position;
+            obj.position = {x: Math.round(n.position.x), y: Math.round(n.position.y)};
             if(n.classes) obj.classes = n.classes;
             nodesSimple.push(obj);
             console.log(nodesSimple);
@@ -31,4 +31,31 @@ export function logJSONOnDrag(cy){
 
 export function isMobile() {
     return window.innerWidth <= 768;
+}
+
+/**
+ * 
+ * @param {*} cy Cytoscape core
+ * @param {*} frequency Adjust this value for faster/slower waves
+ * @param {*} amplitude Adjust this value for larger/smaller waves
+ */
+export function animateGraph(cy, frequency = 0.0001, amplitude = 0.02){
+    function animateNodes() {
+      
+        cy.nodes().forEach((node, i) => {
+          const originalPosition = node.position(); // Get the original position
+      
+          const dx = Math.sin(performance.now() * frequency + i) * amplitude;
+          const dy = Math.cos(performance.now() * frequency + i) * amplitude;
+      
+          node.position({
+            x: originalPosition.x + dx,
+            y: originalPosition.y + dy
+          });
+        });
+      
+        requestAnimationFrame(animateNodes); // Continue the animation
+      }
+      
+      requestAnimationFrame(animateNodes); // Start the animation
 }
